3c1bda9155d2f0a09d7e02ad6a1212cdbec7e41e
Paweł Kita <kitapawel@gmail.com>
2021-09-13 08:17:41 +0200

    Dialogue editor completed

--- RPG Project Base/Assets/_Scripts/Dialogue/DialogueNode.cs
+++ RPG Project Base/Assets/_Scripts/Dialogue/DialogueNode.cs
@@ -1,11 +1,81 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-[System.Serializable]
-public class DialogueNode
+namespace RPG.Dialogue
 {
-    public string uniqueID;
-    public string text;
-    public string[] children;
+    public class DialogueNode : ScriptableObject
+    {
+        [SerializeField]
+        bool isPlayerSpeaking = false;
+        [SerializeField]
+        string text;
+        [SerializeField]
+        List<string> children = new List<string>();
+        [SerializeField]
+        Rect rect = new Rect(0, 0, 200, 100);
+
+        public Rect GetRect()
+        {
+            return rect;
+        }
+
+        public string GetText()
+        {
+            return text;
+        }
+
+        public List<string> GetChildren()
+        {
+            return children;
+        }
+
+        public bool IsPlayerSpeaking()
+        {
+            return isPlayerSpeaking;
+        }
+
+#if UNITY_EDITOR
+        public void SetPosition(Vector2 newPosition)
+        {
+            Undo.RecordObject(this, "Move Dialogue Node");
+            rect.position = newPosition;
+            EditorUtility.SetDirty(this);
+        }
+
+        public void SetText(string newText)
+        {
+            if (newText != text)
+            {
+                Undo.RecordObject(this, "Update Dialogue Text");
+                text = newText;
+                EditorUtility.SetDirty(this);
+            }
+        }
+
+        public void AddChild(string childID)
+        {
+            Undo.RecordObject(this, "Add Dialogue Link");
+            children.Add(childID);
+            EditorUtility.SetDirty(this);
+        }
+
+        public void RemoveChild(string childID)
+        {
+            Undo.RecordObject(this, "Remove Dialogue Link");
+            children.Remove(childID);
+            EditorUtility.SetDirty(this);
+        }
+
+        public void SetPlayerSpeaking(bool newIsPlayerSpeaking)
+        {
+            Undo.RecordObject(this, "Change Dialogue Speaker");
+            isPlayerSpeaking = newIsPlayerSpeaking;
+            EditorUtility.SetDirty(this);
+        }
+
+#endif
+    }
 }
