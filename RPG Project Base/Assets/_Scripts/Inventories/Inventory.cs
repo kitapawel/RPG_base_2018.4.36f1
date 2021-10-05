22d58497796ed5c1b402ffac6a7d16021d1819e8
Paweł Kita <kitapawel@gmail.com>
2021-10-05 12:51:41 +0200

    QUEST section completed.

--- RPG Project Base/Assets/_Scripts/Inventories/Inventory.cs
+++ RPG Project Base/Assets/_Scripts/Inventories/Inventory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using RPG.Saving;
+using RPG.Core;
 
 namespace RPG.Inventories
 {
@@ -10,7 +11,7 @@ namespace RPG.Inventories
     ///
     /// This component should be placed on the GameObject tagged "Player".
     /// </summary>
-    public class Inventory : MonoBehaviour, ISaveable
+    public class Inventory : MonoBehaviour, ISaveable, IPredicateEvaluator
     {
         // CONFIG DATA
         [Tooltip("Allowed size")]
@@ -225,7 +226,7 @@ namespace RPG.Inventories
             public string itemID;
             public int number;
         }
-    
+
         object ISaveable.CaptureState()
         {
             var slotStrings = new InventorySlotRecord[inventorySize];
@@ -252,6 +253,17 @@ namespace RPG.Inventories
             {
                 inventoryUpdated();
             }
+        }
+
+        public bool? Evaluate(string predicate, string[] parameters)
+        {
+            switch (predicate)
+            {
+                case "HasInventoryItem":
+                    return HasItem(InventoryItem.GetFromID(parameters[0]));
+            }
+
+            return null;
         }
     }
 }
